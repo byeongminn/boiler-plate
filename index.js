4e1f7f824cd1cee180e3c547578315cd628da04a
DESKTOP-TVPJ660\user <sekn1997@naver.com>
2021-08-14 13:59:01 +0900

    비밀번호 암호화 & 로그인 기능

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const config = require("./config/key");
+const { User } = require('./models/User');
 
 const mongoose = require("mongoose");
 mongoose.connect(config.mongoURI, {
@@ -14,6 +15,43 @@ mongoose.connect(config.mongoURI, {
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+app.post("/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "입력한 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 일치하는지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." });
+
+      // 비밀번호까지 일치한다면 토큰을 생성한다.
+      user.generateToken((err, user) => {
+        
+      })
+    })
+  })
+
+
+
+
+})
 
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!');
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10   // salt가 몇 글자인지 나타냄
 
 const userSchema = mongoose.Schema({
     name: {
@@ -26,6 +28,31 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre("save", function (next) {     // pre : mongoose 메소드로, ~하기 전에 실행한다는 의미
+    let user = this;
+    
+    if (user.isModified("password")) {
+        // 비밀번호를 암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    })
+}
 
 const User = mongoose.model("User", userSchema);
 
