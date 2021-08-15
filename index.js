c3aa3336d64cbe0744335b8c321ef11a3db6fd31
DESKTOP-TVPJ660\user <sekn1997@naver.com>
2021-08-15 13:47:33 +0900

    로그아웃

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post("/api/users/login", (req, res) => {
   })
 })
 
-app.get("/api/users/auth", auth , (req, res) => {
+app.get("/api/users/auth", auth, (req, res) => {
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
@@ -68,6 +68,13 @@ app.get("/api/users/auth", auth , (req, res) => {
     email: req.user.email,
     name: req.user.name,
     role: req.user.role
+  })
+})
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
   })
 })
 
