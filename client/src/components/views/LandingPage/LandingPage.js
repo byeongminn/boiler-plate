d1b6548a796079125ba0b27eacfcc84dff5caf0d
DESKTOP-TVPJ660\user <sekn1997@naver.com>
2021-08-16 19:03:11 +0900

    로그아웃 & 인증체크

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,18 +1,30 @@
 import React, { useEffect } from 'react';
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
     useEffect(() => {
         axios.get("/api/hello")
         .then(response => console.log(response.data));
     }, [])
+
+    const onClick = () => {
+        axios.get("/api/users/logout")
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push("/login");
+                } else {
+                    alert("로그아웃에 실패하였습니다.");
+                }
+            })
+    }
 
     return (
         <div style={{
             display: "flex", justifyContent: "center", alignItems: "center"
             , width: "100%", height: "100vh"
         }}>
-            LandingPage
+            <h2>LandingPage</h2>
+            <button onClick={onClick}>로그아웃</button>
         </div>
     )
 }
