9989e0329a3b88e4dc7fb2d5f7a7ed676f34f992
DESKTOP-TVPJ660\user <sekn1997@naver.com>
2021-08-16 15:33:18 +0900

    로그인

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,7 +8,10 @@ function LandingPage() {
     }, [])
 
     return (
-        <div>
+        <div style={{
+            display: "flex", justifyContent: "center", alignItems: "center"
+            , width: "100%", height: "100vh"
+        }}>
             LandingPage
         </div>
     )
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
 
-function LoginPage() {
+function LoginPage(props) {
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (event) => {
+        const {
+            target: { name, value }
+        } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        let body = {
+            email,
+            password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if (response.payload.loginSuccess) {
+                    props.history.push("/");
+                } else {
+                    alert("Error");
+                }
+            })
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: "flex", justifyContent: "center", alignItems: "center"
+            , width: "100%", height: "100vh"
+        }}>
+            <form style={{
+                display: "flex", flexDirection: "column"
+            }} onSubmit={onSubmit} >
+                <label>Email</label>
+                <input name="email" type="email" value={email} onChange={onChange} />
+                <label>Password</label>
+                <input name="password" type="password" value={password} onChange={onChange} />
+                <br />
+                <button type="submit">
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
